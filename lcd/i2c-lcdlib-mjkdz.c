53175ca9cb7f3e084ff5a677efcacf855511bf68
oasis3855 <oasis3855@users.noreply.github.com>
2013-02-03 10:39:57 +0000

    EEPROMライブラリ 新規
    git-svn-id: svn+ssh://oasis.halfmoon.jp/home/tmg1136-inue/var/svn/pic-i2c-lib@2 43063ad1-266d-e211-a4ce-001d9241b766

--- lcd/i2c-lcdlib-mjkdz.c
+++ lcd/i2c-lcdlib-mjkdz.c
@@ -1,5 +1,5 @@
 /*
- * File:   i2c-lcdlib-mjkdz.c   for Microchip PIC XC8
+ * File:   i2c-lcdlib-mjkdz.c   for Microchip PIC XC8 (12F1822, 16F1823, 16F1827)
  *
  * I2C LCD 変換アダプタ mjkdz I2C lcd1602用
  */
@@ -12,8 +12,8 @@
 #include "i2c-lcdlib-mjkdz.h"
 
 #ifndef _XTAL_FREQ
-	/* 例：4MHzの場合、4000000 をセットする */
-	#define _XTAL_FREQ 4000000
+    /* 例：4MHzの場合、4000000 をセットする */
+    #define _XTAL_FREQ 4000000
 #endif
 
 // I2C Bus Control Definition
@@ -35,10 +35,10 @@
 
 // send raw data to I2C LCD (I2C LCDにデータを送信する低レベル関数)
 void i2c_lcd_send_common(const unsigned char data){
-	i2c_start();
-	i2c_send_byte((LCD_I2C_ADDR<<1) | I2C_WRITE_CMD);	// アドレスを1ビット左にシフトし、末尾にR/Wビット（Write=0）を付与
-	i2c_send_byte(data);
-	i2c_stop();
+    i2c_start();
+    i2c_send_byte((LCD_I2C_ADDR<<1) | I2C_WRITE_CMD);   // アドレスを1ビット左にシフトし、末尾にR/Wビット（Write=0）を付与
+    i2c_send_byte(data);
+    i2c_stop();
     }
 
 // send command to I2C LCD (I2C LCDにコマンドデータを送信する) 4 bit mode
@@ -141,34 +141,31 @@ void i2c_lcd_init(void){
 
 // put one character on LCD (LCDに1文字表示する)
 void i2c_lcd_putch(const unsigned char c){
-	i2c_lcd_send_data(c);					//文字表示
+    i2c_lcd_send_data(c);                   //文字表示
 }
 
 // put string on LCD (LCDに文字列を表示する)
 void i2c_lcd_puts(const unsigned char *s){
-	while(*s)				//文字取り出し
-		i2c_lcd_send_data(*s++);			//文字表示
+    while(*s)               //文字取り出し
+        i2c_lcd_send_data(*s++);            //文字表示
 }
 
 // send command to I2C LCD (I2C液晶にコマンドを送信する)
-void i2c_lcd_cmd(const unsigned char cmd)
-{
-	i2c_lcd_send_command(cmd, LCD_2CYCLE);
+void i2c_lcd_cmd(const unsigned char cmd){
+    i2c_lcd_send_command(cmd, LCD_2CYCLE);
     __delay_us(100);    // Wait 0.037ms (HD44780U, SD1602V)
 }
 
 // clear screen of I2C LCD
-void i2c_lcd_clear(void)
-{
-	i2c_lcd_send_command(LCD_CLEAR, LCD_2CYCLE);
+void i2c_lcd_clear(void){
+    i2c_lcd_send_command(LCD_CLEAR, LCD_2CYCLE);
     __delay_ms(2);    // Wait 1.52ms (SD1602V)
 }
 
 // set cursor position of I2C LCD (I2C液晶 カーソル位置の指定)
-void i2c_lcd_set_cursor_pos(const unsigned char pos)
-{
-	// line one : pos=0 ...
-	// line two : pos=0x40 ...
-	i2c_lcd_send_command(0x80 | pos, LCD_2CYCLE);
+void i2c_lcd_set_cursor_pos(const unsigned char pos){
+    // line one : pos=0 ...
+    // line two : pos=0x40 ...
+    i2c_lcd_send_command(0x80 | pos, LCD_2CYCLE);
     __delay_us(100);    // Wait 0.037ms (HD44780U, SD1602V)
 }
